79f1ebb389dc637daf8a102fd1b6b51a2c8967f3
chhincke <chhincke@ncsu.edu>
2024-11-06 18:23:21 -0500

    [javadoc] created test javadoc for IO

--- Project2/test/edu/ncsu/csc216/wolf_tracker/model/io/ProjectReaderTest.java
+++ Project2/test/edu/ncsu/csc216/wolf_tracker/model/io/ProjectReaderTest.java
@@ -4,8 +4,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+/**
+ * Tests the ProjectReader class
+ * @author Cole Hincken
+ */
 class ProjectReaderTest {
 
+	/**
+	 * Tests the readProjectFile method
+	 */
 	@Test
 	void testReadProjectFile() {
 		fail("Not yet implemented");
--- Project2/test/edu/ncsu/csc216/wolf_tracker/model/io/ProjectWriterTest.java
+++ Project2/test/edu/ncsu/csc216/wolf_tracker/model/io/ProjectWriterTest.java
@@ -4,13 +4,23 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+/**
+ * Tests the projectWriter class
+ */
 class ProjectWriterTest {
 
+	
+	/**
+	 * Tests the writeProjectFile method
+	 */
 	@Test
 	void testWriteProjectFile() {
 		fail("Not yet implemented");
 	}
 
+	/**
+	 * Tests the writeStatsFile() method
+	 */
 	@Test
 	void testWriteStatsFile() {
 		fail("Not yet implemented");
