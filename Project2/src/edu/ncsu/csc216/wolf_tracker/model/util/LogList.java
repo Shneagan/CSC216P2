38d6686ae3a0f0b079f3e519893f7368a198be24
chhincke <chhincke@ncsu.edu>
2024-11-06 16:55:29 -0500

    [javadoc] added javadoc to util

--- Project2/src/edu/ncsu/csc216/wolf_tracker/model/util/LogList.java
+++ Project2/src/edu/ncsu/csc216/wolf_tracker/model/util/LogList.java
@@ -16,36 +16,63 @@ public class LogList<E> implements ILogList<E> {
 	/** the intial capacity of the array */
 	private static final int INIT_CAP = 10;
 	
+	/**
+	 * Adds an element to the list
+	 * @param element the element to be added to the list
+	 */
 	@Override
 	public void addLog(E element) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	/**
+	 * Sets an element at a specific index in the log, replacing what was once in that index
+	 * @param idx index to be added at
+	 * @param element value to be added
+	 */
 	@Override
 	public void setLog(int idx, E element) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	/**
+	 * removes a value from the list at a specific index, the list is then shifted to adjust and remove the empty space
+	 * @param idx the index where the value will be removed
+	 * @return the value that is removed
+	 */
 	@Override
 	public E removeLog(int idx) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * gets the value at a specific index in the list (log)
+	 * @param idx the index where the value we need is
+	 * @return the value in the specific index
+	 */
 	@Override
 	public E getLog(int idx) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * gets the size of the list
+	 * @return integer number that represents the size of the list
+	 */
 	@Override
 	public int size() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 	
+	/**
+	 * sets the capacity of the list
+	 * @param capacity the new maximum amount of values that can be added to the list
+	 */
 	private void ensureCapacity(int capacity) {
 		
 	}
--- Project2/src/edu/ncsu/csc216/wolf_tracker/model/util/SortedList.java
+++ Project2/src/edu/ncsu/csc216/wolf_tracker/model/util/SortedList.java
@@ -1,36 +1,59 @@
 package edu.ncsu.csc216.wolf_tracker.model.util;
 
-public class SortedList<E extends Comparable<E>> implements ISortedList<E>{
+/**
+ * A custom list where values in the list are automatically sorted. In this case, tasks are sorted based on their names
+ * @param <E> the element type to be added. 
+ */
+public class SortedList<E extends Comparable<E>> implements ISortedList<E> {
 
+	/** the amount of elements in the list */
 	private int size;
 	
+	/**
+	 * Adds an element to the list, and is automatically sorted
+	 * @param element the value to be added
+	 */
 	@Override
 	public void add(E element) {
-		// TODO Auto-generated method stub
 		
 	}
 
+	/**
+	 * removes an element from the list at the index parameter provided
+	 * @param idx the index provided
+	 * @return the element that is removed
+	 */
 	@Override
 	public E remove(int idx) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * Checks if the list contains the parameter value
+	 * @param element the value that we are checking for inside the list
+	 * @return true if the element is in the list, false otherwise
+	 */
 	@Override
 	public boolean contains(E element) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
+	/**
+	 * gets the value that is at the index provided in the parameter
+	 * @param idx the index provided
+	 * @return the value at the index
+	 */
 	@Override
 	public E get(int idx) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * gets the size of the list
+	 * @return integer value of the number of elements in the list
+	 */
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
